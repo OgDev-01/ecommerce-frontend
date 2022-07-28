b9a9c7ee5e5d8d40500cf6df81dc14855abd41a4
Og-Dev <oliviamegan11@gmail.com>
2022-07-28 09:44:58 +0100

    [fix]: button props fix for 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,16 +1,18 @@
-import Layout from "@/components/organisms/Layouts/Layout";
-import React from "react";
-import ComponentNotFound from "../components/organisms/Pages/ComponentNotFound/ComponentNotFound";
+import Layout from '@/components/organisms/Layouts/Layout';
+import React from 'react';
+import ComponentNotFound from '../components/organisms/Pages/ComponentNotFound/ComponentNotFound';
 
 const PageError = () => {
   const spec = {
-    text: "Home Page",
-    urlPath: "/",
+    text: 'Home Page',
+    urlPath: '/',
     icon: {
-      url: "/images/icons/arrow-left.svg",
-      alt: "arrow right",
-      position: "left",
+      url: '/images/icons/arrow-left.svg',
+      alt: 'arrow right',
+      position: 'left',
     },
+    type: 'link',
+    variants: 'default',
   };
   return (
     <Layout>
